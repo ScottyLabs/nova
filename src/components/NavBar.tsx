a0317c77314fe178a0606a01dfb2bb6784ebad38
Gabriel Hall <adrastopoulos@gmail.com>
2024-03-13 01:48:45 -0400

    feat: landing page (desktop)

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+
+const links = [
+	{ name: 'About', link: '#about' },
+	{ name: 'Schedule', link: '#schedule' },
+	{ name: 'Speakers', link: '#speakers' },
+	{ name: 'Prizes', link: '#prizes' },
+	{ name: 'Sponsors', link: '#sponsors' },
+	{ name: 'FAQ', link: '#faq' },
+] as const;
+
+export default function NavBar() {
+	return (
+		<nav className="m-8">
+			<div className="flex justify-between">
+				<Image src="/logo.svg" alt="Nova" width={100} height={100} />
+				<div className="flex items-center justify-center gap-8 font-paradisio capitalize">
+					{links.map((link) => (
+						<a key={link.name} href={link.link}>
+							<span className="tracking-widest capitalize group transition duration-300">
+								{link.name.toUpperCase()}
+								<span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-primary"></span>
+							</span>
+						</a>
+					))}
+				</div>
+			</div>
+		</nav>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      background: '#000000',
+      text: '#CEFFD9',
+      primary: '#41F168',
+      tertiary: '#FD39C3',
+      quaternary: '#FF3C00',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        paradisio: ['var(--font-paridisio)'],
+        diatype: ['var(--font-diatype)'],
+        'stretch-sans': ['var(--font-stretch-sans)']
+      }
     },
   },
   plugins: [],
