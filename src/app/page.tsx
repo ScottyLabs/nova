f3b7bf49b446a7da1a541ded2b967133d834b266
GhostOf0days <jaisal.patel45@gmail.com>
2024-03-20 21:20:53 -0400

    move schedule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
 		<main className="flex flex-col gap-8">
 			<LandingSection />
 			<Separator />
-			<ComingSoonSection />
 			<Schedule />
+			<ComingSoonSection />
 			<AboutSection />
 		</main>
 	);
